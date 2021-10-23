5d49563f0db8fc438ae99d1d0130c0e75e8a3dda
skeka <shreker240@gmail.com>
2021-10-23 20:12:35 +0300

    flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+import json
+
+app = Flask(__name__)
+
+with open("data.json") as file:
+    data = json.load(file)
+
+
+@app.route("/")
+def calendar():
+    return render_template("main.jinja", schedule=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
